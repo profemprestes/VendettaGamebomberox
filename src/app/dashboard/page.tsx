26272510db0f800261d81609bb244cf54c69b3b3
Matias Bombe <bomberox@enviosdosruedas.com>
2025-12-29 03:38:52 +0000

    Quitar este componente específico de la página principal del juego  (_fo

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,11 @@
 import { Changelog } from './changelog'
 import { Notices } from './notices'
-import { TopPlayers } from './top-players'
 
 export default async function DashboardPage() {
   return (
     <div className="grid grid-cols-1 gap-8">
       <Changelog />
       <Notices />
-      <TopPlayers />
     </div>
   )
 }
