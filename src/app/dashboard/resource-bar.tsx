e524c4464c56ec110803368ac72e9efb82c2ecb5
Matias Bombe <bomberox@enviosdosruedas.com>
2025-12-29 06:15:46 +0000

    Para resource bar en dispositivos móviles utilizar para nombres de los r

--- src/app/dashboard/resource-bar.tsx
+++ src/app/dashboard/resource-bar.tsx
@@ -1,12 +1,12 @@
 'use client'
 
-import { Coins, Droplets, RussianRuble, Shell } from "lucide-react";
+import { Coins, Droplets, Shell } from "lucide-react";
 
 const resources = [
-    { name: "Armas", value: "10.000", icon: <Shell className="w-5 h-5 text-gray-500" /> },
-    { name: "Municion", value: "10.000", icon: <Coins className="w-5 h-5 text-yellow-500" /> },
-    { name: "Alcohol", value: "10.000", icon: <Droplets className="w-5 h-5 text-blue-400" /> },
-    { name: "Dolares", value: "10.000", icon: <p className="text-lg font-bold text-green-500">$</p> },
+    { name: "Armas", shortName: "A", value: "10.000", icon: <Shell className="w-5 h-5 text-gray-500" /> },
+    { name: "Municion", shortName: "M", value: "10.000", icon: <Coins className="w-5 h-5 text-yellow-500" /> },
+    { name: "Alcohol", shortName: "A", value: "10.000", icon: <Droplets className="w-5 h-5 text-blue-400" /> },
+    { name: "Dolares", shortName: "D", value: "10.000", icon: <p className="text-lg font-bold text-green-500">$</p> },
 ]
 
 export function ResourceBar() {
@@ -16,7 +16,10 @@ export function ResourceBar() {
                 {resources.map(resource => (
                     <div key={resource.name} className="bg-stone-700/80 p-2">
                         <div className="flex items-center justify-between">
-                            <span className="text-sm font-bold text-stone-400">{resource.name}:</span>
+                            <div className="text-sm font-bold text-stone-400">
+                                <span className="hidden md:inline">{resource.name}:</span>
+                                <span className="md:hidden">{resource.shortName}:</span>
+                            </div>
                             <div className="flex items-center gap-2">
                                 <span className="text-sm font-mono">{resource.value}</span>
                                 {resource.icon}
